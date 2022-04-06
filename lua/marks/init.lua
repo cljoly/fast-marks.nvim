2bddf5992e74dd55be51f88d41c81f0cbf262584
Clément Joly <l@131719.xyz>
2022-04-06 20:45:11 +0100

    fix: remove the marks.vim file

--- lua/marks/init.lua
+++ lua/marks/init.lua
@@ -144,6 +144,38 @@ local function setup_autocommands()
   augroup end]]
 end
 
+local function plugin_init()
+  if vim.fn.exists("g:loaded_marks") then
+    return
+  end
+  g.loaded_marks = 1
+
+  vim.cmd [[
+    hi default link MarkSignHL Identifier
+    " hi default link MarkSignLineHL Normal
+    hi default link MarkSignNumHL CursorLineNr
+    hi default link MarkVirtTextHL Comment
+
+    command! -nargs=? MarksToggleSigns silent lua require'marks'.toggle_signs(<args>)
+    command! MarksListBuf exe "lua require'marks'.mark_state:buffer_to_list()" | lopen
+    command! MarksListGlobal exe "lua require'marks'.mark_state:global_to_list()" | lopen
+    command! MarksListAll exe "lua require'marks'.mark_state:all_to_list()" | lopen
+    command! MarksQFListBuf exe "lua require'marks'.mark_state:buffer_to_list('quickfixlist')" | copen
+    command! MarksQFListGlobal exe "lua require'marks'.mark_state:global_to_list('quickfixlist')" | copen
+    command! MarksQFListAll exe "lua require'marks'.mark_state:all_to_list('quickfixlist')" | copen
+
+    nnoremap <Plug>(Marks-set) <cmd> lua require'marks'.set()<cr>
+    nnoremap <Plug>(Marks-setnext) <cmd> lua require'marks'.set_next()<cr>
+    nnoremap <Plug>(Marks-toggle) <cmd> lua require'marks'.toggle()<cr>
+    nnoremap <Plug>(Marks-delete) <cmd> lua require'marks'.delete()<cr>
+    nnoremap <Plug>(Marks-deleteline) <cmd> lua require'marks'.delete_line()<cr>
+    nnoremap <Plug>(Marks-deletebuf) <cmd> lua require'marks'.delete_buf()<cr>
+    nnoremap <Plug>(Marks-preview) <cmd> lua require'marks'.preview()<cr>
+    nnoremap <Plug>(Marks-next) <cmd> lua require'marks'.next()<cr>
+    nnoremap <Plug>(Marks-prev) <cmd> lua require'marks'.prev()<cr>
+  ]]
+end
+
 function M.setup(config)
   M.mark_state = mark.new()
   M.mark_state.builtin_marks = config.builtin_marks or {}
@@ -180,6 +212,8 @@ function M.setup(config)
 
   local timer = vim.loop.new_timer()
   timer:start(0, refresh_interval, vim.schedule_wrap(M.refresh))
+
+  plugin_init()
 end
 
 return M
